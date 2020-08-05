e749d5bcc2310564750902d8ed324f10acbd83e4
Marco Yuen <yuenmarco17@gmail.com>
2020-08-05 14:48:53 -0700

    Moved single function of JavaScript to index.html

--- script.js
+++
@@ -1,6 +0,0 @@
-function copyToClipboard() {
-    const textToCopy = document.getElementById('email').innerHTML;
-    navigator.clipboard.writeText(textToCopy);
-    
-    alert('\nMy email has been copied to your clipboard.\nI look forward to speaking with you soon!\n\n- Marco');
-}
